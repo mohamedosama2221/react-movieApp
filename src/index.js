e532b3f03f8107688cc425e9f88bb75d36a8c59d
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-10-10 11:22:52 +0400

    adding global style and theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./style/Theme";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}></ThemeProvider>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
