7b885b76acf44dc08ed7f991f0fcfc0ccd5f95fe
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-10-10 12:01:05 +0400

    finished the nav style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav } from "./Header.style";
+import { Nav, Links, Wrapper, Button } from "./Header.style";
 
 const Header = () => {
   return (
-    <div className="container">
-      <Nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            NetFlix
-          </Link>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success">Search</button>
-          </form>
-        </div>
-      </Nav>
-    </div>
+    <Wrapper as="section">
+      <div className="container">
+        <Nav className="navbar navbar-light bg-light">
+          <div className="container-fluid">
+            <Link to="/movie/3" className="navbar-brand">
+              <Links>NetFlix</Links>
+            </Link>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <Button className="btn btn-outline-success">Search</Button>
+            </form>
+          </div>
+        </Nav>
+      </div>
+    </Wrapper>
   );
 };
 
