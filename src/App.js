100345ab9312045430da312c4294f7320448fdec
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-10-10 10:29:20 +0400

    applying routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Home from "./components/Home/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
