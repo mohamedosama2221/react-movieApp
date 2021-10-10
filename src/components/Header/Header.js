39f09a6e815a6d23ed2f7e3e36f001b4bd71c5be
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-10-10 10:58:55 +0400

    Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Nav } from "./Header.style";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="container">
+      <Nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            NetFlix
+          </Link>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success">Search</button>
+          </form>
+        </div>
+      </Nav>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background-color: #000 !important;
+`;
+export const Links = styled.a`
+  color: #000 !important;
+`;
